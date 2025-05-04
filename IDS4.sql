7726dd6cebe494a2cd413f6d00fab0ff3b3522f0
Jackie <mvtjr289@gmail.com>
2025-05-04 06:46:44 +0200

    * commented most of the code * ordered it in a logical order

--- IDS4.sql
+++ IDS4.sql
@@ -14,6 +14,10 @@ DROP SEQUENCE lounge_num;
 DROP SEQUENCE bill_num;
 DROP SEQUENCE order_seq;
 
+---------------------------------------------------------------------------------
+--======================= CREATE TABLES FOR OBJECTS =============================
+---------------------------------------------------------------------------------
+
 CREATE TABLE Person (
     person_id VARCHAR(11) PRIMARY KEY, -- 'YYMMDD/XXXX' format
     name VARCHAR(20),
@@ -62,6 +66,72 @@ CREATE TABLE reservation (
     )
 );
 
+CREATE TABLE bill_tab (
+    tab_id INT PRIMARY KEY,
+    table_id INT,
+    lounge_id INT,
+    sum DECIMAL(10, 2) DEFAULT 0,
+    FOREIGN KEY (table_id) REFERENCES tTable(table_id),
+    FOREIGN KEY (lounge_id) REFERENCES lounge(lounge_id),
+    CHECK (
+        (table_id IS NOT NULL AND lounge_id IS NULL) OR
+        (table_id IS NULL AND lounge_id IS NOT NULL)
+    )
+);
+--- Automatic PK generation if none provided
+--- start
+CREATE SEQUENCE order_seq
+START WITH 1
+INCREMENT BY 1;
+
+CREATE TABLE tOrder (
+    "order_id" INT PRIMARY KEY,
+    date_time DATE,
+    tab_id INT NOT NULL,
+    employee_id VARCHAR(11),
+    FOREIGN KEY (tab_id) REFERENCES bill_tab(tab_id),
+    FOREIGN KEY (employee_id) REFERENCES Employee(person_id)
+);
+
+CREATE TABLE tProduct (
+    product_id VARCHAR(9) PRIMARY KEY,
+    name VARCHAR(20),
+    price DECIMAL(10, 2)
+);
+
+CREATE TABLE order_item (
+    "order_id" INTEGER,
+    product_id VARCHAR(50),
+    quantity INTEGER NOT NULL,
+    PRIMARY KEY ("order_id", product_id),
+    FOREIGN KEY ("order_id") REFERENCES tOrder("order_id"),
+    FOREIGN KEY (product_id) REFERENCES tProduct(product_id)
+);
+
+
+---------------------------------------------------------------------------------
+--======================= RIGHTS TO TABLES (and wrongs)==========================
+---------------------------------------------------------------------------------
+GRANT ALL ON Customer TO xhajekj00; 
+
+---------------------------------------------------------------------------------
+--=============================== TRIGGERS ======================================
+---------------------------------------------------------------------------------
+/**
+ * @brief inserts an valid order id, had it not been provided
+*/
+CREATE OR REPLACE TRIGGER trg_order_id
+BEFORE INSERT ON tOrder
+FOR EACH ROW
+WHEN (NEW."order_id" IS NULL)
+BEGIN
+    SELECT order_seq.NEXTVAL INTO :NEW."order_id" FROM dual;
+END;
+/
+
+/**
+ * @brief checks for an already existing/conflicting reservation 
+*/
 CREATE OR REPLACE TRIGGER trg_reservation_id
 BEFORE INSERT ON reservation
 FOR EACH ROW
@@ -93,59 +163,10 @@ BEGIN
 END;
 /
 
-CREATE TABLE bill_tab (
-    tab_id INT PRIMARY KEY,
-    table_id INT,
-    lounge_id INT,
-    sum DECIMAL(10, 2) DEFAULT 0,
-    FOREIGN KEY (table_id) REFERENCES tTable(table_id),
-    FOREIGN KEY (lounge_id) REFERENCES lounge(lounge_id),
-    CHECK (
-        (table_id IS NOT NULL AND lounge_id IS NULL) OR
-        (table_id IS NULL AND lounge_id IS NOT NULL)
-    )
-);
---- Automatic PK generation if none provided
---- start
-CREATE SEQUENCE order_seq
-START WITH 1
-INCREMENT BY 1;
-
-CREATE TABLE tOrder (
-    "order_id" INT PRIMARY KEY,
-    date_time DATE,
-    tab_id INT NOT NULL,
-    employee_id VARCHAR(11),
-    FOREIGN KEY (tab_id) REFERENCES bill_tab(tab_id),
-    FOREIGN KEY (employee_id) REFERENCES Employee(person_id)
-);
-
-CREATE OR REPLACE TRIGGER trg_order_id
-BEFORE INSERT ON tOrder
-FOR EACH ROW
-WHEN (NEW."order_id" IS NULL)
-BEGIN
-    SELECT order_seq.NEXTVAL INTO :NEW."order_id" FROM dual;
-END;
-/
-
-
---- end
-CREATE TABLE tProduct (
-    product_id VARCHAR(9) PRIMARY KEY,
-    name VARCHAR(20),
-    price DECIMAL(10, 2)
-);
-
-CREATE TABLE order_item (
-    "order_id" INTEGER,
-    product_id VARCHAR(50),
-    quantity INTEGER NOT NULL,
-    PRIMARY KEY ("order_id", product_id),
-    FOREIGN KEY ("order_id") REFERENCES tOrder("order_id"),
-    FOREIGN KEY (product_id) REFERENCES tProduct(product_id)
-);
-
+/**
+ * @brief updates tabs sum when order_item is updated/created for an order related to 
+ the tab
+*/
 CREATE OR REPLACE TRIGGER trg_order_sum_to_tab
 BEFORE INSERT ON order_item
 FOR EACH ROW
@@ -162,11 +183,14 @@ BEGIN
     WHERE tab_id = (SELECT tab_id FROM tOrder WHERE "order_id" = :NEW."order_id");
 END;
 /
-------------------------------------------------------------------
--- RIGHTS (and wrongs)
-GRANT ALL ON Customer TO xhajekj00; 
-------------------------------------------------------------------
--- Simple procedures to insert Employees and Customers // existence of person_id not checked !!!
+
+---------------------------------------------------------------------------------
+--============================== PROCEDURES =====================================
+---------------------------------------------------------------------------------
+/**
+ * @brief inserts both person and employee into the system
+ * Does not handle already existing peolpe
+*/
 CREATE OR REPLACE PROCEDURE insert_employee (
     p_person_id IN VARCHAR2,
     p_name IN VARCHAR2,
@@ -183,6 +207,10 @@ BEGIN
 END;
 /
 
+/**
+ * @brief inserts both person and customer into the system
+ * Does not handle already existing people !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
 CREATE OR REPLACE PROCEDURE insert_customer (
     p_person_id IN VARCHAR2,
     p_name IN VARCHAR2,
@@ -198,12 +226,32 @@ BEGIN
     VALUES (p_person_id, p_loyalty_tier);
 END;
 /
-------------------------------------------------------------------
 
--- TEST DATA
-------------------------------------------------------------------
--- Insert Employees and Customers
+/**
+ * @brief this procedure deletes all orders that belong to a specific tab
+ * Possible use-case is for when the tab is paid, the information about it's
+ * contents are no longer needed.
+*/
+CREATE OR REPLACE PROCEDURE proc_delete_orders(
+    p_tab_id IN bill_tab.tab_id%TYPE) 
+    AS
+    v_order tOrder%ROWTYPE;
 
+    CURSOR c_orders IS
+        SELECT * FROM tOrder WHERE tab_id = p_tab_id;
+BEGIN
+    FOR v_order IN c_orders LOOP
+        DELETE FROM order_item WHERE "order_id" = v_order."order_id";
+        DELETE FROM tOrder WHERE "order_id" = v_order."order_id";
+    END LOOP;
+
+    DBMS_OUTPUT.put_line('Orders for tab_id : ' || p_tab_id);
+END;
+/
+
+---------------------------------------------------------------------------------
+--=========================== INSERT TEST DATA ==================================
+---------------------------------------------------------------------------------
 BEGIN
     insert_employee('921231/4343', 'Jackie Welles', '+420934929422', 'JackWels@arasaka.com', 'Head Cheff');
     insert_employee('640902/6646', 'Keanu Reeves', '+903303429443', 'KenRev@matrix.com','Security');
@@ -242,11 +290,39 @@ INSERT INTO tTable VALUES (tTable_num.NEXTVAL, 4);
 INSERT INTO tTable VALUES (tTable_num.NEXTVAL, 5);
 INSERT INTO tTable VALUES (tTable_num.NEXTVAL, 5);
 
---------------------------------------------------------------------------
-----------------------TEST ORDERS-----------------------------------------
---------------------------------------------------------------------------
--- If customer pays the bill, all related orders are deleted from the system
+-- Reservations
+INSERT INTO reservation (
+    reservation_id,
+    date_time,
+    number_of_persons,
+    duration,
+    customer_id,
+    table_id,
+    lounge_id
+) VALUES (
+    1,
+    TO_DATE('2025-03-30 19:00:00', 'YYYY-MM-DD HH24:MI:SS'),
+    2,
+    120,  -- minutes (2 hours)
+    '600411/1212',  -- Jeremy
+    1, -- Table 1
+    NULL
+);
 
+INSERT INTO reservation VALUES (
+    2, TO_DATE('2025-04-30 16:00:00', 'YYYY-MM-DD HH24:MI:SS'),
+    2, 120, '630116/1213',  3, NULL
+);
+
+INSERT INTO reservation VALUES (
+    3, TO_DATE('2025-04-01 20:30:00', 'YYYY-MM-DD HH24:MI:SS'),
+    8, 180, '691219/4242',  NULL, 2
+);
+
+INSERT INTO reservation VALUES (
+    4, TO_DATE('2025-06-01 20:30:00', 'YYYY-MM-DD HH24:MI:SS'),
+    8, 180, '691219/4242',  NULL, 2  -- lounge 2
+);
 
 -- Tabs
 CREATE SEQUENCE bill_num START WITH 1 INCREMENT BY 1;
@@ -277,15 +353,16 @@ VALUES (200, TO_DATE('2025-03-31 06:46:40', 'YYYY-MM-DD HH24:MI:SS'), 3, '640107
 INSERT INTO order_item ("order_id", product_id, quantity) 
 VALUES (1, '000000004', 1); -- Lasagna 1x
 
-SELECT * FROM bill_tab --======================================================================
+---------------------------------------------------------------------------------
+--================== trg_order_sum_to_tab - DEMONSTRATION =======================
+---------------------------------------------------------------------------------
+SELECT * FROM bill_tab
 WHERE tab_id = 1;
 
 INSERT INTO order_item VALUES (1, '000000007', 2); -- Kofola 2x
 INSERT INTO order_item VALUES (1, '000000008', 1); -- Water 1x
--- INSERT INTO order_item VALUES (1, '000000007', 2); 
-    -- Kofola 2x would be weird, make new order or update
 
-SELECT * FROM bill_tab --======================================================================
+SELECT * FROM bill_tab
 WHERE tab_id = 1;
 
 -- Order items second order - tab2 - table2
@@ -298,106 +375,42 @@ INSERT INTO order_item VALUES (3, '000000007', 3); -- Kofola 3x,
 INSERT INTO order_item VALUES (4, '000000004', 2); -- Lasagna 3x,
 INSERT INTO order_item VALUES (4, '000000007', 6); -- Kofola 6x,
 
---------------------------------------------------------------------------
-----------------------TEST RESERVATIONS-----------------------------------
---------------------------------------------------------------------------
-INSERT INTO reservation (
-    reservation_id,
-    date_time,
-    number_of_persons,
-    duration,
-    customer_id,
-    table_id,
-    lounge_id
-) VALUES (
-    1,
-    TO_DATE('2025-03-30 19:00:00', 'YYYY-MM-DD HH24:MI:SS'),
-    2,
-    120,  -- minutes (2 hours)
-    '600411/1212',  -- Jeremy
-    1, -- Table 1
-    NULL
-);
+---------------------------------------------------------------------------------
+--====================== delete_orders -- DEMNOSTRATION =========================
+---------------------------------------------------------------------------------
+SELECT
+    o.tab_id,
+    SUM(oi.quantity * tp.price) AS "SUM",
+    bt.table_id,
+    bt.lounge_id
+FROM tOrder o
+JOIN order_item oi ON o."order_id" = oi."order_id"
+JOIN tProduct tp ON oi.product_id = tp.product_id
+JOIN bill_tab bt ON o.tab_id = bt.tab_id
+GROUP BY o.tab_id, bt.table_id, bt.lounge_id;
 
-INSERT INTO reservation VALUES (
-    2,
-    TO_DATE('2025-04-30 16:00:00', 'YYYY-MM-DD HH24:MI:SS'),
-    2,
-    120,  -- minutes (2 hours)
-    '630116/1213',  -- May
-    3, -- Table 3
-    NULL
-);
 
-INSERT INTO reservation VALUES (
-    3,
-    TO_DATE('2025-04-01 20:30:00', 'YYYY-MM-DD HH24:MI:SS'),
-    8,
-    180,  -- 3 hours
-    '691219/4242',  -- Richard
-    NULL,
-    2  -- lounge 2
-);
+BEGIN
+    proc_delete_orders(2);
+END;
+/
 
-INSERT INTO reservation VALUES (
-    4,
-    TO_DATE('2025-06-01 20:30:00', 'YYYY-MM-DD HH24:MI:SS'),
-    8,
-    180,  -- 3 hours
-    '691219/4242',  -- Richard
-    NULL,
-    2  -- lounge 2
-);
---------------------------------------------------------------------------
+SELECT
+    o.tab_id,
+    SUM(oi.quantity * tp.price) AS "SUM",
+    bt.table_id,
+    bt.lounge_id
+FROM tOrder o
+JOIN order_item oi ON o."order_id" = oi."order_id"
+JOIN tProduct tp ON oi.product_id = tp.product_id
+JOIN bill_tab bt ON o.tab_id = bt.tab_id
+GROUP BY o.tab_id, bt.table_id, bt.lounge_id;
+
 COMMIT;
 
-CREATE OR REPLACE PROCEDURE delete_order_cascade(
-    p_tab_id IN bill_tab.tab_id%TYPE) 
-    AS
-    v_order tOrder%ROWTYPE;
-
-    CURSOR c_orders IS
-        SELECT * FROM tOrder WHERE tab_id = p_tab_id;
-BEGIN
-    FOR v_order IN c_orders LOOP
-        DELETE FROM order_item WHERE "order_id" = v_order."order_id";
-        DELETE FROM tOrder WHERE "order_id" = v_order."order_id";
-    END LOOP;
-
-    DBMS_OUTPUT.put_line('Orders for tab_id : ' || p_tab_id);
-END;
-/
-
-SELECT
-    o.tab_id,
-    SUM(oi.quantity * tp.price) AS "SUM",
-    bt.table_id,
-    bt.lounge_id
-FROM tOrder o
-JOIN order_item oi ON o."order_id" = oi."order_id"
-JOIN tProduct tp ON oi.product_id = tp.product_id
-JOIN bill_tab bt ON o.tab_id = bt.tab_id
-GROUP BY o.tab_id, bt.table_id, bt.lounge_id;
-
-
-BEGIN
-    delete_order_cascade(2); --- delete orders related to bill  
-END;
-/
-
-SELECT * FROM bill_tab;
-
-SELECT
-    o.tab_id,
-    SUM(oi.quantity * tp.price) AS "SUM",
-    bt.table_id,
-    bt.lounge_id
-FROM tOrder o
-JOIN order_item oi ON o."order_id" = oi."order_id"
-JOIN tProduct tp ON oi.product_id = tp.product_id
-JOIN bill_tab bt ON o.tab_id = bt.tab_id
-GROUP BY o.tab_id, bt.table_id, bt.lounge_id;
-
+---------------------------------------------------------------------------------
+--======================== EXPLAIN PLAN DEMNOSTRATION ===========================
+---------------------------------------------------------------------------------
 EXPLAIN PLAN FOR
 SELECT * FROM reservation WHERE table_id = 1 AND date_time > SYSDATE;
 
@@ -409,3 +422,22 @@ EXPLAIN PLAN FOR
 SELECT * FROM reservation WHERE table_id = 1 AND date_time > SYSDATE;
 
 SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+
+/* PLAN_TABLE_OUTPUT
+---------------------------------------------------------------------------------
+| Id  | Operation         | Name        | Rows  | Bytes | Cost (%CPU)| Time     |
+---------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT  |             |     1 |    81 |     3   (0)| 00:00:01 |
+|*  1 |  TABLE ACCESS FULL| RESERVATION |     1 |    81 |     3   (0)| 00:00:01 |
+---------------------------------------------------------------------------------
+ 
+PLAN_TABLE_OUTPUT
+----------------------------------------------------------------------------------------------------------
+| Id  | Operation                           | Name               | Rows  | Bytes | Cost (%CPU)| Time     |
+----------------------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT                    |                    |     1 |    81 |     0   (0)| 00:00:01 |
+|   1 |  TABLE ACCESS BY INDEX ROWID BATCHED| RESERVATION        |     1 |    81 |     0   (0)| 00:00:01 |
+|*  2 |   INDEX RANGE SCAN                  | IDX_RES_TABLE_DATE |     1 |       |     0   (0)| 00:00:01 |
+----------------------------------------------------------------------------------------------------------
+-- CPU cost has been reduced
+*/
